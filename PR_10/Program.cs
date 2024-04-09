b998751d72871eee462dc6a5e1eb9b252adf2303
dimaz <dima.zadoyan2005@gmail.com>
2024-04-09 13:51:36 +0300

    Add project files.

---
+++ PR_10/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace PR_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введіть адресу електронної пошти: ");
+            string email = Console.ReadLine();
+
+            bool isValid = Regex.IsMatch(email, @"^[^@\s]+@[^@\s]+\.[^@\s]+$");
+
+            
+            if (isValid)
+            {
+                Console.WriteLine("Адреса електронної пошти дійсна");
+            }
+            else
+            {
+                Console.WriteLine("Адреса електронної пошти не дійсна");
+            }
+            Console.ReadLine();
+        }
+    }
+}
